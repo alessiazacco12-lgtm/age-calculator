98fdc245af0cef7b16fe5a730a058259c7861ca6
alessia <alessia.zacco12@gmail.com>
2026-08-18 17:56:36 +0200

    Initial Angular project setup

---
+++ src/app/app.ts
@@ -0,0 +1,18 @@
+// Importa Component, necessario per creare un componente Angular.
+import { Component } from '@angular/core';
+
+// Importa il componente AgeCalculator.
+import { AgeCalculator } from './components/age-calculator/age-calculator';
+
+@Component({
+  // Nome del componente principale usato nell'HTML.
+  selector: 'app-root',
+
+  // Rende disponibile AgeCalculator dentro questo componente.
+  imports: [AgeCalculator],
+  // Collega il file HTML del componente.
+  templateUrl: './app.html',
+  // Collega il file CSS del componente.
+  styleUrl: './app.css'
+})
+export class App {}
---
+++ src/app/components/age-calculator/age-calculator.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-age-calculator',
+  imports: [],
+  templateUrl: './age-calculator.html',
+  styleUrl: './age-calculator.css',
+})
+export class AgeCalculator {}
